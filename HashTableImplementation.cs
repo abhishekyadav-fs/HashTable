a4af358d8d8ee4859f07852b98558a1737a6960f
animeshy2015 <45916659+animeshy2015@users.noreply.github.com>
2018-12-16 19:42:15 +0530

    Add files via upload

---
+++ HashTableImplementation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Custom Implementaion of HashTable
+/// </summary>
+namespace HashTable
+{
+
+    /// <summary>
+    /// This hastable is implemented to store only string value with integer keys 
+    /// </summary>
+    public class HashTableImplementation
+    {
+        private string[] bucket;
+
+
+        /// <summary>
+        /// Indexer to provide array like encapsulation to hashtable
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string this[int key]
+        {
+            get {
+                int index = HashFunc(key) % bucket.Length;
+                return this.bucket[index];
+            }
+            set {
+                int index = HashFunc(key) % bucket.Length;
+                bucket[index] = value;
+            }
+        }
+        // Creating bucket with initial size 1000        
+        public HashTableImplementation(){
+            this.bucket = new string[1000];
+        }
+        //Method to compute the hash
+        private int HashFunc(int key)
+        {
+            int hash = key % 2069;
+            return hash;
+        }
+        /// <summary>
+        /// Method to add values to bucket
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        
+        public void Add(int key,string value)
+        {
+            if (value == null)
+            {
+                throw new NotImplementedException();
+            }
+            
+            int index = HashFunc(key) % bucket.Length;
+            bucket[index] = value;
+        }
+
+        public bool ContainsKey(int key)
+        {
+            int hash = HashFunc(key);
+            int index = hash % bucket.Length;
+            if(bucket[index]!=null || String.IsNullOrEmpty(bucket[index])) { return true; }
+            else { return false; }
+        }
+    }
+}
