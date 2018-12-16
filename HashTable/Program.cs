78745de3db1dd16763734c1f44a4eee41bced2b2
Abhishek Yadav <abhikgeek@users.noreply.github.com>
2018-12-16 19:27:22 +0530

    Add files via upload

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -5,30 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HashTableDemo
+namespace HashTable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string s = "abacdfrab";
-            Version v = new Version(5,56,17,0);
-            Console.WriteLine( v.Build);
-            Dictionary<char, int> dt = new Dictionary<char, int>();
-            for(char c = 'a'; c <= 'z'; c++)
-            {                
-                dt.Add(c, 0);
-            }
-            for(int i = 0; i < s.Length; i++)
-            {
-                dt[s[i]]++;
-            }
-            for (char c = 'a'; c <= 'z'; c++)
-            {
-                Console.WriteLine(c +" - " +dt[c]);
-            }
-            Console.ReadKey();
+            Console.WriteLine("################### HashTable Implementation #########################");
 
+            Console.Write("How many keys your want to enter : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = n;
+            while (i > 0)
+            {
+                Console.Write("Please enter the key :");
+                int key = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Please enter the value :");
+                string value = Console.ReadLine();
+                HashTableImplementation ht = new HashTableImplementation();
+                ht.Add(key, value);                
+                i--;
+            }            
+            Console.ReadKey();
         }
     }
 }
