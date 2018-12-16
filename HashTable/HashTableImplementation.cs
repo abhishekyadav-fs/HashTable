dea5ed4f6f16cffe29150422c6229749cf4fdf9c
Abhishek Yadav <abhikgeek@users.noreply.github.com>
2018-12-16 18:33:05 +0530

    Add files via upload

---
+++ HashTable/HashTableImplementation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class HashTableImplementation
+    {
+        private string[] bucket;
+
+        //Method to compute the hash
+        private int HashFunc(int key)
+        {
+            int hash = key % 2069;
+            return hash;
+        }
+
+        public void Add(int key,string value)
+        {
+
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abacdfrab";
+            Version v = new Version(5,56,17,0);
+            Console.WriteLine( v.Build);
+            Dictionary<char, int> dt = new Dictionary<char, int>();
+            for(char c = 'a'; c <= 'z'; c++)
+            {                
+                dt.Add(c, 0);
+            }
+            for(int i = 0; i < s.Length; i++)
+            {
+                dt[s[i]]++;
+            }
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.WriteLine(c +" - " +dt[c]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
